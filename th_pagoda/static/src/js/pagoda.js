14a815c052f1d51c334202d8464bd7d1b83feef5
Sonnb1999 <103470213+Sonnb1999@users.noreply.github.com>
2023-05-16 14:12:47 +0700

    update
    ....

--- th_pagoda/static/src/js/pagoda.js
+++ th_pagoda/static/src/js/pagoda.js
@@ -1,41 +1,65 @@
 /** @odoo-module **/
 
 
-const { Component, useState, onWillStart, onWillUnmount } = owl;
-import { registry } from "@web/core/registry";
+const {Component, useState, onWillStart, onWillUnmount, onWillUpdateProps, onWillPatch} = owl;
+import {registry} from "@web/core/registry";
 import session from 'web.session';
 import rpc from 'web.rpc';
 
-class PagodaHomepage extends Component{
+class PagodaHomepage extends Component {
     setup() {
         super.setup();
         this.state = useState({
-            count: 0
+            count: 0,
+            all_user:[]
         });
+        this.current_class = '';
         this.uid = session.user_id[0];
-        onWillStart(async ()=>{
-            return Promise.all([this.fetch_data()]);
+        onWillStart(async () => {
+            window.addEventListener('beforeunload', this._onBeforeUnload);
+            return Promise.all([this.fetch_data(), this.search_user()]);
+
         });
-        this.saveInterval = setInterval(this.save_data.bind(this), 5000);
+        // this.saveInterval = setInterval(this.save_data.bind(this), 5000);
+        this.refreshUserInterval = setInterval(this.search_user.bind(this), 10000);
 
-
-        onWillUnmount(() =>{
-           clearInterval(this.saveInterval);
+        onWillUpdateProps(async (nextProps) => {
+            debugger
+            // this.fieldInfo = await this.loadField(nextProps.resModel, nextProps.node.operands[0]);
+        });
+        onWillPatch(() => {
+            debugger
+            this.scrollState = this.getScrollSTate();
         });
 
+
+        onWillUnmount(() => {
+            clearInterval(this.saveInterval);
+            clearInterval(this.refreshUserInterval);
+        });
 
     }
-    increase () {
-        this.state.count ++;
+
+    stopPropagation(){
+        debugger
     }
 
-    onChangeState(){
-        if(this.state.count% 10 != 0) return
+    getScrollSTate(){
+        return 1
+    }
+
+    increase() {
+        this.state.count++;
+        this.current_class = this.current_class ? '' : 'click_stick';
+    }
+
+    onChangeState() {
+        debugger
+        if (this.state.count % 10 != 0) return
         this.save_data()
     }
 
-
-    async fetch_data(){
+    async fetch_data() {
         let result = await rpc.query({
             model: 'res.users',
             method: 'search_read',
@@ -45,15 +69,31 @@ class PagodaHomepage extends Component{
         this.state.count = result[0]['pg_check_click']
 
     }
-    async save_data(){
+
+    async save_data() {
         rpc.query({
-               model: 'res.users',
-               method: 'write',
-               args: [this.uid, {pg_check_click: this.state.count}]
-           });
+            model: 'res.users',
+            method: 'write',
+            args: [this.uid, {pg_check_click: this.state.count}]
+        });
+
+    }
+
+    async search_user() {
+        let result = await rpc.query({
+            model: 'res.users',
+            method: 'search_read',
+            args: [[]],
+            fields: ['name', 'pg_check_click'],
+            limit: 10,
+        });
+        result.sort((a,b) => b.pg_check_click - a.pg_check_click);
+
+        this.state.all_user = result
     }
 
 
 }
+
 PagodaHomepage.template = "th_pagoda.PagodaHomepage";
 registry.category("actions").add("th_pagoda", PagodaHomepage);
