412d8c3d325d97f78f895132212fc2ffc0d96de9
Sonnb1999 <103470213+Sonnb1999@users.noreply.github.com>
2023-07-26 15:17:07 +0700

    update

--- th_affiliate/models/res_config_setting.py
+++ th_affiliate/models/res_config_setting.py
@@ -11,8 +11,21 @@ _interval_types = {
     'days': lambda interval: relativedelta(days=interval),
 }
 
+
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    th_access_interval_number = fields.Integer(default=7, tracking=True, help="")
+    th_access_interval_number = fields.Integer(string="Thời gian tồn tại", tracking=True)
     th_access_interval_type = fields.Selection(list(_interval_selection.items()), default='days', tracking=True)
+
+    def set_values(self):
+        super().set_values()
+        self.env['ir.config_parameter'].set_param('th_access_interval_number', self.th_access_interval_number)
+        self.env['ir.config_parameter'].set_param('th_access_interval_type', self.th_access_interval_type)
+
+    @api.model
+    def get_values(self):
+        res = super().get_values()
+        res.update(th_access_interval_number=self.env['ir.config_parameter'].sudo().get_param('th_access_interval_number'),
+                   th_access_interval_type=self.env['ir.config_parameter'].sudo().get_param('th_access_interval_type'), )
+        return res
