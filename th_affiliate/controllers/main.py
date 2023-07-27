7fa8effaf795db9af5557ba4f21f521caa97e814
Sonnb1999 <103470213+Sonnb1999@users.noreply.github.com>
2023-07-27 17:05:23 +0700

    Update main.py
    Cần có header trên server để local có thể call tránh bị cors

--- th_affiliate/controllers/main.py
+++ th_affiliate/controllers/main.py
@@ -4,13 +4,32 @@ from datetime import time
 
 from werkzeug.exceptions import NotFound
 
-from odoo import http
+from odoo import http, api
 from odoo.http import request, Response
 from odoo.addons.link_tracker.controller.main import LinkTracker
+import json
 
 
 class ThLinkTracker(LinkTracker):
-    pass
+    @http.route('/api/check_cookie', type='http', auth='none')
+    def check_cookie(self, **kwargs):
+        setting = request.env['res.config.settings'].get_values()
+        th_access_interval_number = setting.get('th_access_interval_number', False)
+        th_access_interval_type = setting.get('th_access_interval_type', False)
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://127.0.0.1:5500',
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Origin"
+        }
+        cookie = {
+            'th_access_interval_number': th_access_interval_number,
+            'th_access_interval_type': th_access_interval_type,
+
+        }
+        body = {'results': {'code': 200, 'message': 'OK 1111111111111111111111'}, 'cookie': cookie}
+        return Response(json.dumps(body), headers=headers)
+
     # @http.route('/r/<string:code>', type='http', auth='public', website=True)
     # def full_url_redirect(self, code, **post):
     #     if not request.env['ir.http'].is_a_bot():
