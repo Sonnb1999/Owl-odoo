8623c1882ac6cbac3eba795dbda9cf4ad637de2d
Sonnb1999 <103470213+Sonnb1999@users.noreply.github.com>
2024-05-31 16:20:00 +0700

    update

--- th_portal_website/models/th_attachment.py
+++ th_portal_website/models/th_attachment.py
@@ -1,14 +1,20 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-
+READONLY_STATES = {
+    'accept': [('readonly', True)],
+    'reject': [('readonly', True)],
+}
 
 class Attachment(models.Model):
     _name = 'th.attachment'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Attachment for Application Forms'
 
-    name = fields.Char('Tên file', required=True, tracking=True)
-    attachment_ids = fields.Many2many('ir.attachment', string='File đính kèm', required=True, tracking=True, copy=False)
+    name = fields.Char('Tên file', required=True, tracking=True, states=READONLY_STATES)
+    attachment_ids = fields.Many2many('ir.attachment', string='File đính kèm', required=True, tracking=True, copy=False, states=READONLY_STATES)
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection([('draft', 'chờ duyệt'), ('accept', 'Chấp thuận'), ('reject', 'Từ chối')], default="draft", tracking=True,)
+
 
     @api.model
     def create(self, values):
@@ -24,3 +30,15 @@ class Attachment(models.Model):
             if len(rec.attachment_ids) > 1:
                 raise ValidationError('1 Đơn không thể có 2 file')
         return res
+
+    def th_action_accept(self):
+        for rec in self:
+            rec.state = 'accept'
+
+    def th_action_reject(self):
+        for rec in self:
+            rec.state = 'reject'
+
+    def th_action_draft(self):
+        for rec in self:
+            rec.state = 'draft'
