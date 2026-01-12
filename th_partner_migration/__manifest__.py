1e65688bbe2f8fc6297f2747e95e1e17eb143ec3
Sonnb1999 <103470213+Sonnb1999@users.noreply.github.com>
2026-01-12 11:40:22 +0700

    test

---
+++ th_partner_migration/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+{
+    'name': 'Migration',
+    'summary': '',
+    'version': '16.0.2.2',
+    'license': 'LGPL-3',
+    'author': 'Nova Code',
+    'website': '',
+    'category': 'Forms/Forms',
+    'depends': [
+        'contacts'
+    ],
+    'application': True,
+    'installable': True,
+    'uninstall_hook': '',
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [],
+        'web.assets_frontend': [],
+        'web.assets_common': [],
+    },
+    'demo': [],
+    'images': []
+}
